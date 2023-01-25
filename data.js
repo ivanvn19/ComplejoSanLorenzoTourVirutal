81c1681b797ec2f3d72e1fc34e18b8025089763c
ivanvn19 <ivan.vazquezneira@gmail.com>
2023-01-25 10:01:30 -0300

    SanLorenzoTourVirtual

---
+++ data.js
@@ -0,0 +1,765 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-frente-complejo",
+      "name": "frente complejo",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.44345610586386464,
+        "pitch": -0.11309295452822887,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6211027498306656,
+          "pitch": 0.09268116616953037,
+          "rotation": 0,
+          "target": "1-frente-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-frente-ii",
+      "name": "frente II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -1.56944526263079,
+        "pitch": -0.027644944440231356,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2738361900415747,
+          "pitch": 0.036674076248409904,
+          "rotation": 0,
+          "target": "2-ingreso-al-edificio-"
+        },
+        {
+          "yaw": 1.176298955499579,
+          "pitch": 0.13344982142503348,
+          "rotation": 0,
+          "target": "0-frente-complejo"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.4346716985645536,
+          "pitch": -0.1570119488106343,
+          "title": "San Lorenzo",
+          "text": "Con un estilo minimalista San Lorenzo es un complejo de departamentos 3 ambientes, completamente equipados ubicado en el centro de San Miguel.<div>El departamento posee toilette en planta baja, patio y cochera.&nbsp;&nbsp;</div>"
+        }
+      ]
+    },
+    {
+      "id": "2-ingreso-al-edificio-",
+      "name": "ingreso al edificio ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.4493276057399473,
+        "pitch": 0.19928013425367652,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.72185254907248,
+          "pitch": 0.1921086761873365,
+          "rotation": 0,
+          "target": "3-ingreso"
+        },
+        {
+          "yaw": -0.36231845715237476,
+          "pitch": 0.07274077648812138,
+          "rotation": 0,
+          "target": "1-frente-ii"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-ingreso",
+      "name": "ingreso",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.3685148743105877,
+        "pitch": 0.33649561913125936,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3655732111160894,
+          "pitch": 0.16441926865018885,
+          "rotation": 0,
+          "target": "6-acceso-escalera"
+        },
+        {
+          "yaw": -2.139187983206167,
+          "pitch": 0.21162808744838024,
+          "rotation": 0,
+          "target": "4-cocina"
+        },
+        {
+          "yaw": 2.8801597128598315,
+          "pitch": 0.25162171351629325,
+          "rotation": 0,
+          "target": "2-ingreso-al-edificio-"
+        },
+        {
+          "yaw": -0.7012195037256639,
+          "pitch": 0.11714148736833785,
+          "rotation": 4.71238898038469,
+          "target": "5-toilette"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cocina",
+      "name": "cocina",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.9331373852538274,
+        "pitch": 0.5804091647375387,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.3249580162286136,
+          "pitch": 0.26016423717746306,
+          "rotation": 0,
+          "target": "3-ingreso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-toilette",
+      "name": "Toilette",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.524766204841878,
+        "pitch": 0.6486770345660169,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.25246076424597774,
+          "pitch": 0.10114905308380351,
+          "rotation": 0,
+          "target": "3-ingreso"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-acceso-escalera",
+      "name": "acceso escalera",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.315811830284722,
+        "pitch": 0.20219363103106325,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23550296956572225,
+          "pitch": 0.03585424535702231,
+          "rotation": 0,
+          "target": "2-ingreso-al-edificio-"
+        },
+        {
+          "yaw": -0.09001779328678516,
+          "pitch": -0.0936876805853597,
+          "rotation": 1.5707963267948966,
+          "target": "4-cocina"
+        },
+        {
+          "yaw": 0.17154018505043211,
+          "pitch": 0.14148197160947262,
+          "rotation": 1.5707963267948966,
+          "target": "5-toilette"
+        },
+        {
+          "yaw": 1.3610544238514954,
+          "pitch": -0.06870783636389355,
+          "rotation": 0,
+          "target": "11-descanso-escalera-"
+        },
+        {
+          "yaw": 2.436922800154748,
+          "pitch": 0.11982037122182909,
+          "rotation": 0,
+          "target": "7-living---comedor"
+        },
+        {
+          "yaw": 2.7669375735881037,
+          "pitch": 0.1366695360829251,
+          "rotation": 0,
+          "target": "15-patio-trasero"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-living---comedor",
+      "name": "Living - comedor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -3.076977502561112,
+        "pitch": 0.11692124716988062,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5387068859711039,
+          "pitch": 0.08881271061452267,
+          "rotation": 0,
+          "target": "6-acceso-escalera"
+        },
+        {
+          "yaw": 0.3409757637579087,
+          "pitch": -0.23711924709691168,
+          "rotation": 0,
+          "target": "11-descanso-escalera-"
+        },
+        {
+          "yaw": 2.969771022439202,
+          "pitch": 0.1780922330951764,
+          "rotation": 0,
+          "target": "15-patio-trasero"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-accesos-planta-alta",
+      "name": "accesos planta alta",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.527675587052583,
+        "pitch": 0.5042865707470625,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6236319581859222,
+          "pitch": 0.2748554994246515,
+          "rotation": 3.141592653589793,
+          "target": "11-descanso-escalera-"
+        },
+        {
+          "yaw": 2.970186109231843,
+          "pitch": 0.17483266020312271,
+          "rotation": 0,
+          "target": "12-habitacin-contrafrente"
+        },
+        {
+          "yaw": -0.08355106132047219,
+          "pitch": 0.12231897595628993,
+          "rotation": 0,
+          "target": "13-habitacin-frente"
+        },
+        {
+          "yaw": 0.2081564476970943,
+          "pitch": 0.14083567621568882,
+          "rotation": 1.5707963267948966,
+          "target": "10-bao-completo-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-balcn",
+      "name": "balcón",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 2.897973834921573,
+        "pitch": 0.4889923938648977,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5118744342791395,
+          "pitch": 0.1577603843157256,
+          "rotation": 0,
+          "target": "12-habitacin-contrafrente"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "10-bao-completo-",
+      "name": "baño completo ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.3241975114804827,
+        "pitch": 0.6109719366782009,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8008909031420988,
+          "pitch": 0.1600482922082005,
+          "rotation": 0,
+          "target": "8-accesos-planta-alta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "11-descanso-escalera-",
+      "name": "descanso escalera ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.5616752706914099,
+        "pitch": 0.4956782016233312,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.27360140602828054,
+          "pitch": 0.4522105686889759,
+          "rotation": 3.141592653589793,
+          "target": "6-acceso-escalera"
+        },
+        {
+          "yaw": 1.0551471347809507,
+          "pitch": -0.38021738640452263,
+          "rotation": 0,
+          "target": "8-accesos-planta-alta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "12-habitacin-contrafrente",
+      "name": "habitación contrafrente",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.409974823226092,
+        "pitch": 0.17175376800683217,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.616193373460467,
+          "pitch": 0.20105676438507913,
+          "rotation": 0,
+          "target": "9-balcn"
+        },
+        {
+          "yaw": -0.19286414284198372,
+          "pitch": 0.11964249365476931,
+          "rotation": 0,
+          "target": "8-accesos-planta-alta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-habitacin-frente",
+      "name": "habitación frente",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -2.072147249007898,
+        "pitch": 0.06300887455624604,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.081965218730491,
+          "pitch": 0.11994425185470625,
+          "rotation": 0,
+          "target": "8-accesos-planta-alta"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "14-accesos-habitacin-y-bao-",
+      "name": "accesos habitación y baño ",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": 0.4398831269192556,
+        "pitch": 0.2564029476335179,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "15-patio-trasero",
+      "name": "patio trasero",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.022056156858607068,
+        "pitch": 0.22819866338812034,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1337729838234303,
+          "pitch": 0.12018491714213475,
+          "rotation": 0,
+          "target": "16-patio-trasero-ii"
+        },
+        {
+          "yaw": -2.0485633944857327,
+          "pitch": 0.24561328044334552,
+          "rotation": 0,
+          "target": "7-living---comedor"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "16-patio-trasero-ii",
+      "name": "patio trasero II",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1344,
+      "initialViewParameters": {
+        "yaw": -0.18165671806471373,
+        "pitch": 0.23121226259103977,
+        "fov": 1.3108730938557924
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.892205654852991,
+          "pitch": 0.08701274965659422,
+          "rotation": 0,
+          "target": "7-living---comedor"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "San Lorenzo Tour virtual ",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
